ed95374f3a1b82748ff8a63f2ef6e54fd63c2a9d
Massimo Grossi <grossi.massimo@gmail.com>
2025-11-19 16:30:30 +0100

    aggiunti componente e modellocomponente a models.py

--- models.py
+++ models.py
@@ -64,9 +64,9 @@ class FileOrdine(db.Model):
     """Modello File Ordini di Acquisto"""
     __tablename__ = 'file_ordini'
 
-class OrdineAcquisto(db.Model):
-    """Modello Ordini di Acquisto"""
-    __tablename__ = 'ordini_acquisto'
+# class OrdineAcquisto(db.Model):
+#     """Modello Ordini di Acquisto"""
+#     __tablename__ = 'ordini_acquisto'
 
     id = db.Column('id_file_ordini_acquisto', db.Integer, primary_key=True)
     anno = db.Column(db.Integer, nullable=False)
@@ -93,9 +93,9 @@ class FileAnagrafica(db.Model):
     """Modello File Anagrafiche (Excel)"""
     __tablename__ = 'file_anagrafiche'
 
-class AnagraficaFile(db.Model):
-    """Modello Anagrafiche File (Excel)"""
-    __tablename__ = 'anagrafiche_file'
+# class AnagraficaFile(db.Model):
+#     """Modello Anagrafiche File (Excel)"""
+#     __tablename__ = 'anagrafiche_file'
 
     id = db.Column('id_file_anagrafiche', db.Integer, primary_key=True)
     anno = db.Column(db.Integer, nullable=False)
@@ -114,6 +114,67 @@ class AnagraficaFile(db.Model):
         return f'<AnagraficaFile {self.marca} - {self.filename}>'
 
 
+class Componente(db.Model):
+    """Modello Anagrafica Componenti (CRU da pipeline anagrafiche)"""
+    __tablename__ = 'componenti'
+
+    cod_componente = db.Column(db.String(100), primary_key=True)
+    cod_componente_norm = db.Column(db.String(100), unique=True, nullable=False, index=True)
+    desc_componente_it = db.Column(db.Text)
+    cod_alt = db.Column(db.String(100))
+    cod_alt_2 = db.Column(db.String(100))
+    pos_no = db.Column(db.String(50))
+    part_no = db.Column(db.String(100))
+    part_name_en = db.Column(db.String(200))
+    part_name_cn = db.Column(db.String(200))
+    part_name_it = db.Column(db.String(200))
+    cod_ean = db.Column(db.String(50))
+    barcode = db.Column(db.String(10))  # 'N', '1', 'Q'
+
+    # Prezzi
+    unit_price_usd = db.Column(db.Numeric(10, 2))
+    unit_price_notra_noiva_netto_eur = db.Column(db.Numeric(10, 2))
+    unit_price_tra_noiva_netto_eur = db.Column(db.Numeric(10, 2))
+    unit_price_public_eur = db.Column(db.Numeric(10, 2))
+
+    stat = db.Column(db.String(50))
+    softech_stat = db.Column(db.String(50))
+
+    # Audit
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_by = db.Column(db.String(80))
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updated_by = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f'<Componente {self.cod_componente}>'
+
+
+class ModelloComponente(db.Model):
+    """Modello relazione Modelli-Componenti (CRUD da pipeline anagrafiche)"""
+    __tablename__ = 'modelli_componenti'
+
+    modello_componente = db.Column(db.String(200), primary_key=True)  # "cod_modello|cod_componente"
+    id_file_anagrafiche = db.Column(db.Integer, db.ForeignKey('anagrafiche_file.id_file_anagrafiche'), nullable=False)
+    cod_modello = db.Column(db.String(100), db.ForeignKey('modelli.cod_modello'), nullable=False)
+    cod_componente = db.Column(db.String(100), db.ForeignKey('componenti.cod_componente'), nullable=False)
+    qta = db.Column(db.Integer, nullable=False)
+
+    # Audit
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_by = db.Column(db.String(80))
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updated_by = db.Column(db.String(80))
+
+    # Relationships
+    file_anagrafica = db.relationship('AnagraficaFile', backref='modelli_componenti')
+    modello = db.relationship('Modello', backref='componenti_associati')
+    componente = db.relationship('Componente', backref='modelli_associati')
+
+    def __repr__(self):
+        return f'<ModelloComponente {self.modello_componente}>'
+    
+    
 class TraceElaborazione(db.Model):
     """Modello Tracciamento Elaborazioni - un file può avere più elaborazioni"""
     __tablename__ = 'trace_elaborazioni'
